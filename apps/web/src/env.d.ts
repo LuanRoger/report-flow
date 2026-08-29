8e616bf5b8a545f104a71fb43121140195c53480
ROG <luanroger.dev@gmail.com>
2026-08-29 10:44:51 -0300

    chore: update env with AI

--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -22,13 +22,19 @@ export type CoercedEnvSchema = {
    */
   ANALYSIS_API_KEY: string;
   
+  /**
+   * **OPENAI_API_KEY** 🔐 _sensitive_  
+   * ![icon](data:image/svg+xml;utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2220%22%20height%3D%2220%22%20viewBox%3D%220%200%2032%2032%22%3E%3Cpath%20fill%3D%22%23808080%22%20d%3D%22M29%2022h-5a2.003%202.003%200%200%201-2-2v-6a2%202%200%200%201%202-2h5v2h-5v6h5ZM18%2012h-4V8h-2v14h6a2.003%202.003%200%200%200%202-2v-6a2%202%200%200%200-2-2m-4%208v-6h4v6Zm-6-8H3v2h5v2H4a2%202%200%200%200-2%202v2a2%202%200%200%200%202%202h6v-8a2%202%200%200%200-2-2m0%208H4v-2h4Z%22%2F%3E%3C%2Fsvg%3E)   
+   */
+  OPENAI_API_KEY: string;
+  
 };
 
-type _CoercedEnvSchema_20a2604c = CoercedEnvSchema;
+type _CoercedEnvSchema_a06b2b1d = CoercedEnvSchema;
 
 declare module 'varlock/env' {
-  export interface TypedEnvSchema extends Readonly<_CoercedEnvSchema_20a2604c> {}
-  export interface PublicTypedEnvSchema extends Readonly<Pick<_CoercedEnvSchema_20a2604c, 'APP_ENV'>> {}
+  export interface TypedEnvSchema extends Readonly<_CoercedEnvSchema_a06b2b1d> {}
+  export interface PublicTypedEnvSchema extends Readonly<Pick<_CoercedEnvSchema_a06b2b1d, 'APP_ENV'>> {}
 }
 
 
@@ -38,17 +44,17 @@ export type EnvSchemaAsStrings = {
       : (CoercedEnvSchema[Property] extends boolean ? ('true' | 'false') : string)
 };
 
-type _EnvSchemaAsStrings_20a2604c = EnvSchemaAsStrings;
+type _EnvSchemaAsStrings_a06b2b1d = EnvSchemaAsStrings;
 declare global {
 
   // add types for global import.meta.env
-  interface ImportMetaEnv extends _EnvSchemaAsStrings_20a2604c {}
+  interface ImportMetaEnv extends _EnvSchemaAsStrings_a06b2b1d {}
   interface ImportMeta {
     readonly env: ImportMetaEnv;
   }
 
   // add types for global process.env
   namespace NodeJS {
-    interface ProcessEnv extends _EnvSchemaAsStrings_20a2604c {}
+    interface ProcessEnv extends _EnvSchemaAsStrings_a06b2b1d {}
   }
 }
